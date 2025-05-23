dd8f098de0a487e92d1ec215227601bf37ae7dc3
kojo <gideonad4@gmail.com>
2025-05-23 01:45:47 +0100

    deploying

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,9 @@ from contextlib import asynccontextmanager
 from app.db.init import init_db
 from app.routes import auth
 from fastapi.middleware.cors import CORSMiddleware
-
+from dotenv import load_dotenv
+import os
+import uvicorn
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,7 +16,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
 
-
+load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,4 +29,13 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.getenv.get("PORT", 8000)),
+        reload=True  # Optional: remove in production
+    )
 
